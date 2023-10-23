5084b16cf09997e99b23386ee32fe03f258c57a2
Anastasia <apushkova@sfedu.ru>
2023-10-24 00:10:03 +0300

    fixed player controller

--- The_Train/Assets/Scripts/PlayerController.cs
+++ The_Train/Assets/Scripts/PlayerController.cs
@@ -26,7 +26,7 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        moveDirection = new Vector2(moveX, moveY); 
+        moveDirection = new Vector2(moveX, moveY).normalized; 
     }
     void Move()
     {
