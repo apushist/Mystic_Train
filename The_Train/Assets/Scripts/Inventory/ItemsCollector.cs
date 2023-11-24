65e6192da8a06d44bc0c6ac3f2b112f725ff6244
StarostIN2000 <72708700+StarostIN2000@users.noreply.github.com>
2023-11-24 14:39:54 +0300

    Add interactable object

---
+++ The_Train/Assets/Scripts/Inventory/ItemsCollector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsCollector : MonoBehaviour
+{
+    [SerializeField] public InventoryItem _neededItem;
+
+    public bool TrySetItem(InventoryItem item)
+    {
+        return item._id == _neededItem._id;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Inventory.instance.InteractWithObject(this);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Inventory.instance.InteractWithObject();
+        }
+    }
+}
