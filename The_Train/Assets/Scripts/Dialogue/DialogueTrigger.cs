6dfb4bb2ee6354dcc352d779e9875daa7f1fd65b
Anastasia <apushkova@sfedu.ru>
2023-11-27 16:21:28 +0300

    update dialogue system + organize scripts

--- The_Train/Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ The_Train/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-    
+    public GameObject dialogueObject;
+    public bool destroyObjectAfter;
 
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (dialogueObject != null)
+        {
+            TriggerDialogue();
+            if(destroyObjectAfter)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
 }
