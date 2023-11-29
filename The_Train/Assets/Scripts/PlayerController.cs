b6d220b4ff7c4d6eb0cb149d3e98574b94f32e15
Anastasia <apushkova@sfedu.ru>
2023-11-29 09:55:50 +0300

    use player animations in scene

--- The_Train/Assets/Scripts/PlayerController.cs
+++ The_Train/Assets/Scripts/PlayerController.cs
@@ -10,14 +10,23 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveDirection;
     public float stepSoundRate = 0.5F;
     public AudioSource stepSound;
+    public GameObject playerTexture;
+
+    private Animator animator;
 
     private float nextStep = 0.0F;
 
-     public bool canMove = true; //чтобы запретить игроку двигаться, когда он в инвентаре
+    public bool canMove = true; //чтобы запретить игроку двигаться, когда он в инвентаре
 
     public static event Action Epressed;
-    // Update is called once per frame
-    void Update()
+
+	private void Start()
+	{
+		animator = playerTexture.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
     {
         // Processing Inputs
         ProcessInputs();
@@ -40,6 +49,7 @@ public class PlayerController : MonoBehaviour
             float moveX = Input.GetAxisRaw("Horizontal");
             float moveY = Input.GetAxisRaw("Vertical");
 
+            PlayAnimation(moveX, moveY);
             moveDirection = new Vector2(moveX, moveY).normalized;
 
             if (Time.time > nextStep && (moveX != 0 || moveY != 0))
@@ -61,4 +71,53 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
+
+    void PlayAnimation(float moveX,float moveY)
+    {
+        if(moveX == 0 && moveY == 0)
+        {
+			animator.SetBool("IsBack", false);
+			animator.SetBool("IsStraight", false);
+			animator.SetBool("IsLeft", false);
+			animator.SetBool("IsRight", false);
+		}
+        else
+        {
+			if (moveY > 0 && moveX == 0)
+			{
+				animator.SetBool("IsBack", true);
+				animator.SetBool("IsStraight", false);
+				animator.SetBool("IsLeft", false);
+				animator.SetBool("IsRight", false);
+			}
+			else
+			{
+				if (moveY < 0 && moveX == 0)
+				{
+					animator.SetBool("IsBack", false);
+					animator.SetBool("IsStraight", true);
+					animator.SetBool("IsLeft", false);
+					animator.SetBool("IsRight", false);
+				}
+                else
+                {
+                    if(moveX > 0)
+                    {
+						animator.SetBool("IsBack", false);
+						animator.SetBool("IsStraight", false);
+						animator.SetBool("IsLeft", false);
+						animator.SetBool("IsRight", true);
+					}
+                    else
+                    {
+						animator.SetBool("IsBack", false);
+						animator.SetBool("IsStraight", false);
+						animator.SetBool("IsLeft", true);
+						animator.SetBool("IsRight", false);
+					}
+                }
+			}
+		}
+		
+	}
 }
