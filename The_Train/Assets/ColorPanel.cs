a077b6ace48f59b91e37fb3825f40fbd761480e1
StarostIN2000 <72708700+StarostIN2000@users.noreply.github.com>
2024-03-23 13:23:12 +0300

    Puzzle

--- The_Train/Assets/ColorPanel.cs
+++ The_Train/Assets/ColorPanel.cs
@@ -1,12 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorPanel : MonoBehaviour
 {
-    public PanelRotateState _state;
+    [SerializeField] public PanelRotateState _state;
+    [SerializeField] public Transform _itemsParent;
+    [HideInInspector] public int[] _itemsIndex;
+    [HideInInspector] public RectTransform[] _itemsObject;
 
+    void Start()
+    {
+        int iters = _itemsParent.childCount;
+        _itemsIndex = new int[iters];
+        _itemsObject = new RectTransform[iters];
+        for (int i = 0; i < iters; i++)
+        {
+            _itemsObject[i] = _itemsParent.GetChild(i).GetComponent<RectTransform>();
+            for (int j = 0; j < iters; j++)
+            {
+                if (ColorPanelsPuzzle.instance._itemsReference[j] == _itemsObject[i].GetComponent<Image>().sprite)
+                {
+                    _itemsIndex[i] = j;
+                }
+            }
+        }
+        
+    }
     internal void NextState()
     {
         if (_state == PanelRotateState.left)
@@ -15,10 +38,7 @@ public class ColorPanel : MonoBehaviour
             _state++;
     }
 
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
--- The_Train/Assets/ColorPanelsPuzzle.cs
+++ The_Train/Assets/ColorPanelsPuzzle.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ColorPanelsPuzzle : PuzzleBase
 {
@@ -11,12 +12,18 @@ public class ColorPanelsPuzzle : PuzzleBase
     [SerializeField] private RectTransform _parentSprite;
     [SerializeField] private RectTransform _dragSpace;
 
-    [SerializeField] private GameObject[] _verticals;
-    [SerializeField] private GameObject[] _gorizontals;
+    [SerializeField] private RectTransform[] _verticals; ////hide
+    [SerializeField] private RectTransform[] _gorizontals; //hide    
+
+    [SerializeField] internal Sprite[] _itemsReference;
+
+    [SerializeField] private Color _defaultColor;
+    [SerializeField] private Color _goodColor;
+    [SerializeField] private Color _badColor;
 
     public static ColorPanelsPuzzle instance;
 
-    Vector2[][] _colorPoints;
+    Vector2[,] _colorPoints;
 
     float _orientWidth = 1920;
     float _orientHeight = 1080;
@@ -30,11 +37,25 @@ public class ColorPanelsPuzzle : PuzzleBase
     Vector2 _draggingOffset;
     bool _inRotation = false;
 
-    private void Start()
+    private void Awake()
     {
         instance = this;
+    }
+    private void Start()
+    {
         canvasParentMin = CanvasToMousePoint(_parentSprite.anchoredPosition + _parentSprite.offsetMin);
         canvasParentMax = CanvasToMousePoint(_parentSprite.anchoredPosition + _parentSprite.offsetMax);
+
+        _colorPoints = new Vector2[_itemsReference.Length, _itemsReference.Length];
+        for(int i = 0; i < _itemsReference.Length; i++)
+        {
+            for (int j = 0; j < _itemsReference.Length; j++)
+            {
+                _colorPoints[i, j] = new Vector2(-1, -1);
+            }
+        }
+        _gorizontals = new RectTransform[_itemsReference.Length];
+        _verticals = new RectTransform[_itemsReference.Length];
     }
     private void Update()
     {
@@ -76,66 +97,247 @@ public class ColorPanelsPuzzle : PuzzleBase
 
     public void GetPanelToDrag(GameObject obj)
     {
-        if (!obj.CompareTag("ColorPanel")) return;
+        if (!obj.CompareTag("ColorPanel") || _draggingPanel!=null) return;
 
+        if (IsInParentRect(V2(Input.mousePosition))&& IsPanelInLogic(obj.GetComponent<RectTransform>()))
+        {
+            var colorPanel = obj.GetComponent<ColorPanel>();
+            if (colorPanel._state == PanelRotateState.right || colorPanel._state == PanelRotateState.left)
+            {
+                var yLerpCount = lerpParentCoordsToCountY(Input.mousePosition.y);
+                if (_gorizontals[_gorizontals.Length - yLerpCount - 1] != null)
+                {
+                    ClearMatrixItemData(yLerpCount, obj.GetComponent<RectTransform>());
+                }
+            }
+            else
+            {
+                var xLerpCount = lerpParentCoordsToCountX(Input.mousePosition.x);
+                if (_verticals[xLerpCount] != null)
+                {
+                    ClearMatrixItemData(xLerpCount, obj.GetComponent<RectTransform>());
+                }
+            }
+            UpdateColors();
+        }
         _draggingOffset = CanvasToMousePoint(obj.GetComponent<RectTransform>().anchoredPosition) - V2(Input.mousePosition);
         _draggingPanel = Instantiate(obj, _dragSpace).GetComponent<RectTransform>();
+        ResetPanelColor(_draggingPanel);
 
         Destroy(obj);
     }
 
     public void DropPanel()
-    {
+    {      
         if (_inRotation)
         {
             StopAllCoroutines();
             _inRotation = false;
         }
         var inst = Instantiate(_draggingPanel, _parentSprite);
-        
+
         if (IsInParentRect(V2(Input.mousePosition)))
         {
             var colorPanel = _draggingPanel.transform.GetComponent<ColorPanel>();
             if (colorPanel._state == PanelRotateState.right || colorPanel._state == PanelRotateState.left)
             {
                 var yLerpCount = lerpParentCoordsToCountY(Input.mousePosition.y);
-                inst.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, lerpParentCoordsY(yLerpCount));
+                if (_gorizontals[_gorizontals.Length - yLerpCount - 1] == null)
+                {
+                    inst.anchoredPosition = new Vector2(0, lerpParentCoordsY(yLerpCount));
+                    SetMatrixItemData(yLerpCount, inst);
+                }
+                else
+                {
+                    inst.anchoredPosition = MousePointToCanvas(V2(Input.mousePosition) + _draggingOffset);
+                }
             }
             else
             {
                 var xLerpCount = lerpParentCoordsToCountX(Input.mousePosition.x);
-                inst.GetComponent<RectTransform>().anchoredPosition = new Vector2(lerpParentCoordsX(xLerpCount), 0);
+                if (_verticals[xLerpCount] == null)
+                {
+                    inst.anchoredPosition = new Vector2(lerpParentCoordsX(xLerpCount), 0);
+                    SetMatrixItemData(xLerpCount, inst);
+                }
+                else
+                {
+                    inst.anchoredPosition = MousePointToCanvas(V2(Input.mousePosition) + _draggingOffset);
+                }
             }
-           
+            UpdateColors();
         }
         else
         {
-            inst.GetComponent<RectTransform>().anchoredPosition = MousePointToCanvas(V2(Input.mousePosition) + _draggingOffset);
+            inst.anchoredPosition = MousePointToCanvas(V2(Input.mousePosition) + _draggingOffset);
         }
 
         Destroy(_draggingPanel.gameObject);
+        if (CheckForWin())
+        {
+            Debug.Log("win");
+        }
     }
+
     public void MovePanel()
     {
         _draggingPanel.position = V2(Input.mousePosition) + _draggingOffset;
     }
 
-    //возвращает кэнввэс координату привязки по номеру
+    public void SetMatrixItemData(int lerpCount, RectTransform obj)
+    {
+        var colorPanel = obj.transform.GetComponent<ColorPanel>();
+        if (colorPanel._state == PanelRotateState.right || colorPanel._state == PanelRotateState.left)
+        {
+            _gorizontals[_gorizontals.Length - lerpCount - 1] = obj;
+            if(colorPanel._state == PanelRotateState.right)
+            {
+                for(int i = 0; i < _gorizontals.Length; i++)
+                {
+                    _colorPoints[i, _gorizontals.Length - lerpCount - 1].x = colorPanel._itemsIndex[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _gorizontals.Length; i++)
+                {
+                    _colorPoints[i, _gorizontals.Length-lerpCount-1].x = colorPanel._itemsIndex[_gorizontals.Length - i - 1];
+                }
+            }
+        }
+        else
+        {
+            _verticals[lerpCount] = obj;
+            if (colorPanel._state == PanelRotateState.up)
+            {
+                for (int i = 0; i < _verticals.Length; i++)
+                {
+                    _colorPoints[lerpCount, i].y = colorPanel._itemsIndex[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _verticals.Length; i++)
+                {
+                    _colorPoints[lerpCount, i].y = colorPanel._itemsIndex[_gorizontals.Length - i - 1];
+                }
+            }
+        }
+    }
+    public void ClearMatrixItemData(int lerpCount, RectTransform obj)
+    {
+        var colorPanel = obj.transform.GetComponent<ColorPanel>();
+        if (colorPanel._state == PanelRotateState.right || colorPanel._state == PanelRotateState.left)
+        {
+            for (int i = 0; i < _gorizontals.Length; i++)
+            {
+                _colorPoints[i, _gorizontals.Length - lerpCount - 1].x = -1;
+            }
+            _gorizontals[_gorizontals.Length - lerpCount - 1] = null;
+        }
+        else
+        {
+
+            for (int i = 0; i < _verticals.Length; i++)
+            {
+                _colorPoints[lerpCount, i].y = -1;
+            }
+
+            _verticals[lerpCount] = null;
+        }
+    }
+
+    public void UpdateColors()
+    {
+        for (int j = 0; j < _itemsReference.Length; j++)
+        {
+            var pan = _gorizontals[j];
+            if (pan == null) continue;
+            var colorPanel = pan.GetComponent<ColorPanel>();
+            if (colorPanel._state == PanelRotateState.right)
+            {
+                for (int i = 0; i < _itemsReference.Length; i++)
+                {
+                    var v2 = _colorPoints[i, j];
+                    if (v2.x == -1 || v2.y == -1)
+                        colorPanel._itemsObject[i].GetComponent<Image>().color = _defaultColor;
+                    else if (v2.x == v2.y)
+                        colorPanel._itemsObject[i].GetComponent<Image>().color = _goodColor;
+                    else
+                        colorPanel._itemsObject[i].GetComponent<Image>().color = _badColor;
+                }
+            }
+            else if (colorPanel._state == PanelRotateState.left)
+            {
+                for (int i = 0; i < _itemsReference.Length; i++)
+                {
+                    var v2 = _colorPoints[i, j];
+                    if (v2.x == -1 || v2.y == -1)
+                        colorPanel._itemsObject[_itemsReference.Length - i - 1].GetComponent<Image>().color = _defaultColor;
+                    else if (v2.x == v2.y)
+                        colorPanel._itemsObject[_itemsReference.Length - i - 1].GetComponent<Image>().color = _goodColor;
+                    else
+                        colorPanel._itemsObject[_itemsReference.Length - i - 1].GetComponent<Image>().color = _badColor;
+                }
+            }
+
+        }
+        for (int j = 0; j < _itemsReference.Length; j++)
+        {
+            var pan = _verticals[j];
+            if (pan == null) continue;
+            var colorPanel = pan.GetComponent<ColorPanel>();
+            if (colorPanel._state == PanelRotateState.up)
+            {
+                for (int i = 0; i < _itemsReference.Length; i++)
+                {
+                    var v2 = _colorPoints[j, i];
+                    if (v2.x == -1 || v2.y == -1)
+                        colorPanel._itemsObject[i].GetComponent<Image>().color = _defaultColor;
+                    else if (v2.x == v2.y)
+                        colorPanel._itemsObject[i].GetComponent<Image>().color = _goodColor;
+                    else
+                        colorPanel._itemsObject[i].GetComponent<Image>().color = _badColor;
+                }
+            }
+            else if (colorPanel._state == PanelRotateState.down)
+            {
+                for (int i = 0; i < _itemsReference.Length; i++)
+                {
+                    var v2 = _colorPoints[j, i];
+                    if (v2.x == -1 || v2.y == -1)
+                        colorPanel._itemsObject[_itemsReference.Length - i - 1].GetComponent<Image>().color = _defaultColor;
+                    else if (v2.x == v2.y)
+                        colorPanel._itemsObject[_itemsReference.Length - i - 1].GetComponent<Image>().color = _goodColor;
+                    else
+                        colorPanel._itemsObject[_itemsReference.Length - i - 1].GetComponent<Image>().color = _badColor;
+                }
+            }
+
+        }
+    }
+    public void ResetPanelColor(RectTransform pan)
+    {
+        var colorPanel = pan.GetComponent<ColorPanel>();
+        for (int i = 0; i < _itemsReference.Length; i++)
+        {
+            colorPanel._itemsObject[i].GetComponent<Image>().color = _defaultColor;
+        }
+    }
+
+    //возвращает кэнввэс координату привязки по номеру для ВЕРТИКАЛЬНОЙ планки
     public float lerpParentCoordsX(int v)
     {
         float parentSizeMouse = (canvasParentMax.x - canvasParentMin.x);
         return (v - 2) * (parentSizeMouse / 5) * _orientWidth / Screen.width;
     }
+    //возвращает кэнввэс координату привязки по номеру для ГОРИЗОНТАЛЬНОЙ планки
     public float lerpParentCoordsY(int v)
     {
         float parentSizeMouse = (canvasParentMax.y - canvasParentMin.y);
         return (v - 2) * (parentSizeMouse / 5) * _orientWidth / Screen.width;
     }
-    public float lerpParentCoordsН(int v)
-    {
-        float parentSizeMouse = (canvasParentMax.y - canvasParentMin.y);
-        return (v - 2) * (parentSizeMouse / 5) * _orientHeight / Screen.height;
-    }
+
     //возвращает номер привязки панели(0-4)
     public int lerpParentCoordsToCountX(float v)
     {
@@ -155,6 +357,31 @@ public class ColorPanelsPuzzle : PuzzleBase
         StartCoroutine(RotatePanelIE());
     }
 
+    private bool IsPanelInLogic(RectTransform r)
+    {
+        foreach(var i in _verticals)
+        {
+            if (i == r) return true;
+        }
+        foreach (var i in _gorizontals)
+        {
+            if (i == r) return true;
+        }
+        return false;
+    }
+    private bool CheckForWin()
+    {
+        for (int i = 0; i < _itemsReference.Length; i++)
+        {
+            for (int j = 0; j < _itemsReference.Length; j++)
+            {
+                var cur = _colorPoints[i, j];
+                if (cur.x != cur.y || cur.x==-1 || cur.y == -1)
+                    return false;
+            }
+        }
+        return true;
+    }
     IEnumerator RotatePanelIE()
     {
         int frames = 9;
