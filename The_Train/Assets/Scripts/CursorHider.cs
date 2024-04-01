96da287a238798b284dc4c675529be9592470674
apushist <114854401+apushist@users.noreply.github.com>
2024-04-02 00:07:28 +0300

    fix cursor

--- The_Train/Assets/Scripts/CursorHider.cs
+++ The_Train/Assets/Scripts/CursorHider.cs
@@ -5,25 +5,34 @@ using UnityEngine;
 public class CursorHider : MonoBehaviour
 {
 	public float updateTime = 1f;
+	public bool InMenu = false;
 
 	private Coroutine co_HideCursor;
 
+	private void Start()
+	{
+		Cursor.visible = true;
+	}
+
 	private void FixedUpdate()
 	{
-		if (Input.GetAxis("Mouse X") == 0 && (Input.GetAxis("Mouse Y") == 0))
+		if (!InMenu)
 		{
-			if (co_HideCursor == null)
+			if (Input.GetAxis("Mouse X") == 0 && (Input.GetAxis("Mouse Y") == 0))
 			{
-				co_HideCursor = StartCoroutine(HideCursor());
+				if (co_HideCursor == null)
+				{
+					co_HideCursor = StartCoroutine(HideCursor());
+				}
 			}
-		}
-		else
-		{
-			if (co_HideCursor != null)
+			else
 			{
-				StopCoroutine(co_HideCursor);
-				co_HideCursor = null;
-				Cursor.visible = true;
+				if (co_HideCursor != null)
+				{
+					StopCoroutine(co_HideCursor);
+					co_HideCursor = null;
+					Cursor.visible = true;
+				}
 			}
 		}
 	}
